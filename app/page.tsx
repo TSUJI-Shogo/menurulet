3715ae8b606f9fa2f5cc00bf04b3389c13e64831
TSUJI-Shogo <scorpii.jabbah@gmail.com>
2026-01-05 17:39:57 +0900

    絞り込み実装

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,12 @@
 import { useEffect } from "react";
 import Papa from "papaparse";
 
+interface Recipe{
+  id: string; //識別用
+  name: string; //料理名
+  category: string; //材料タイプ
+}
+
 export default function Home() {
   // CSV読み込み
   useEffect(() => {
@@ -10,7 +16,13 @@ export default function Home() {
       .then((response) => response.text())
       .then((csvData) => {
         const parsed = Papa.parse(csvData, { header: true });
-        console.log("解析されたデータ:", parsed.data);
+        const recipes = parsed.data as Recipe[];
+        console.log("絞り込みなし:", parsed.data);
+
+        const category: string = "鶏肉";
+        const filtered_recipes: Recipe[] = recipes.filter((recipe: Recipe) => recipe.category === category);
+        
+        console.log("絞り込みあり:",filtered_recipes);
       });
   }, []);
 
